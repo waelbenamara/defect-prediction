88576ca7006c51cf508193a0bdeccc82116d7c73
waelbenamara <waelbenamara@Waels-MacBook-Air.local>
2021-10-30 16:23:05 -0400

    initial structure

---
+++ predef-v2/metric-extractor/Metric_Extractor.py
@@ -0,0 +1,47 @@
+class Metric_Extractor(object):
+	"""docstring for Metric_Extractor"""
+	def __init__(self,filename):
+		super(Metric_Extractor, self).__init__()
+		with open(filename) as fobj:
+	            self.sourceFile = fobj.read()
+	        self.rawMetrics = self.generateRawMetrics()
+	        self.mcCabeMetrics = self.cyclomaticComplexity()
+	        self.halsteadMetrics = self.generateHalsteadMetrics()
+	def cyclomaticComplexity(self):
+        cc = cc_visit(self.sourceFile)
+        return average_complexity(cc)
+
+    def generateRawMetrics(self):
+        raw = analyze(self.sourceFile)
+        return raw
+
+    def generateHalsteadMetrics(self):
+        hl = h_visit(self.sourceFile)
+        return hl
+
+    def numberOfOperators(self):
+        return self.halsteadMetrics.total[0]
+
+    def numberOfOperands(self):
+        return self.halsteadMetrics.total[1]
+
+    def totalNumberOfOperators(self):
+        return self.halsteadMetrics.total[2]
+
+    def totalNumberOfOperands(self):
+        return self.halsteadMetrics.total[3]
+
+    def halsteadProgramLength(self):
+        return self.halsteadMetrics.total[5]
+
+    def linesOfCode(self):
+        return self.rawMetrics.lloc
+
+    def volume(self):
+        return self.halsteadMetrics.total[7]
+
+    def effort(self):
+        return self.halsteadMetrics.total[9]
+
+    def linesOfComments(self):
+        return self.rawMetrics.comments
